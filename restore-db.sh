851b9eeecd1b0c428729bfd421e6eba9a74020c5
Jhon Santana <jhonsstn@gmail.com>
2023-01-31 18:29:56 -0300

    feat: update app name references

--- restore-db.sh
+++ restore-db.sh
@@ -2,4 +2,4 @@
 
 docker cp ./db-seed db:/tmp/db-seed
 
-docker exec -it db bash -c "mongorestore -u root -p 12345678 --authenticationDatabase admin --db=allugator /tmp/db-seed"
+docker exec -it db bash -c "mongorestore -u root -p 12345678 --authenticationDatabase admin --db=alugaphone /tmp/db-seed"
